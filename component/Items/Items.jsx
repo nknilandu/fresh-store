3b7278caed0a303a640b95c8d774f0b1b233083e
nknilandu <nknilandu@gmail.com>
2026-02-14 23:29:20 +0600

    loading fake data with suspense

---
+++ component/Items/Items.jsx
@@ -0,0 +1,42 @@
+import React, { use } from 'react';
+import { IoMdAdd } from 'react-icons/io';
+
+const Items = ({data}) => {
+
+    const itemData = use(data)
+    // console.log(itemData)
+
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-between mt-8'>
+            {
+                itemData.map((item)=> (
+                    <div className='p-3 bg-[#f3f8f2] rounded-2xl w-full shadow hover:shadow-xl transition-all'>
+                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
+                              <img src={item.image_url} alt="" className='object-cover'/>
+                          </div>
+                          <div>
+                              <p className='text-xl text-black mt-2'>{item.name}</p>
+                              <p className='text-black'>{item.category}</p>
+                              <div className='flex justify-between my-2'>
+                                  <p className='text-3xl font-semibold text-black'>${item.price}<span className='text-lg font-normal text-black/50'>/{item.unit}</span></p>
+                                  <div className='bg-[#10b981] p-2 rounded-full'>
+                                      <IoMdAdd size={25} className='text-white'/>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default Items;
+
+
+
+//  <div >
+      
+
+
+{/*  */}
--- page/AllProduct/AllProduct.jsx
+++ page/AllProduct/AllProduct.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import veggies from "../../src/assets/product/images-donut.jpg"
-import { IoMdAdd } from 'react-icons/io';
+import React, { Suspense } from "react";
+import Items from "../../component/Items/Items";
+
 
 const AllProduct = () => {
+
+    // without async await
+    // const data = fetch("./product.json").then(res=>res.json())
+    // console.log(data)
+
+
+    //with async way
+    const productData = async ()=> {
+        const data = await fetch("./product.json")
+        const res = await data.json()
+        return res
+    }
+    const data = productData()
+
+
+
+
   return (
     <div className="max-w-7xl mx-auto p-5 text-black">
       <div className="flex flex-col md:flex-row gap-3 justify-between items-center">
@@ -34,219 +51,9 @@ const AllProduct = () => {
         </div>
       </div>
       {/* ======================= */}
-                  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-between mt-8'>
-      
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className='p-3 bg-[#f3f8f2] rounded-2xl w-fit shadow hover:shadow-xl transition-all'>
-                          <div className='h-[160px] w-full rounded-2xl overflow-hidden '>
-                              <img src={veggies} alt="" className='object-cover'/>
-                          </div>
-                          <div>
-                              <p className='text-xl text-black mt-2'>Organic Spinach</p>
-                              <p className='text-black'>1 Bunch</p>
-                              <div className='flex justify-between my-2'>
-                                  <p className='text-3xl font-semibold text-black'>$15.50<span className='text-lg font-normal text-black/50'>/bunch</span></p>
-                                  <div className='bg-[#10b981] p-2 rounded-full'>
-                                      <IoMdAdd size={25} className='text-white'/>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      </div>
+                  <Suspense fallback={<span className=" loading loading-spinner text-primary"></span>}>
+                    <Items data={data}></Items>
+                  </Suspense>
 
     </div>
   );
